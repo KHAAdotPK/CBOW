6674ba03c20258e9c7493f7e1b4f64e7cbac2087
KHAAdotPK <spysee.pk@gmail.com>
2025-01-29 00:35:09 +0500

    Update main.hh: Define SKIP_GRAM_CONTEXT_WINDOW_SIZE and format COMMAND string
    - Add SKIP_GRAM_CONTEXT_WINDOW_SIZE macro with value 2
    
    - Reformat COMMAND string definition with line continuations for better readability

--- usage/main.hh
+++ usage/main.hh
@@ -12,6 +12,11 @@
 
 #define CBOW_DEFAULT_CORPUS_FILE ".\\data\\corpus.txt"
 
+#ifdef SKIP_GRAM_CONTEXT_WINDOW_SIZE
+#undef SKIP_GRAM_CONTEXT_WINDOW_SIZE
+#endif
+#define SKIP_GRAM_CONTEXT_WINDOW_SIZE 2
+
 #ifdef GRAMMAR_END_OF_TOKEN_MARKER
 #undef GRAMMAR_END_OF_TOKEN_MARKER
 #endif
@@ -32,6 +37,12 @@
 
 #include "../lib/WordEmbedding-Algorithms/Word2Vec/CBOW/header.hh"
 
-#define COMMAND "h -h help --help ? /? (Displays help screen)\nv -v version --version /v (Displays version number)\ne epoch --epoch /e (Sets epoch or number of times the training loop would run)\ncorpus --corpus (Path to the file which has the training data)\nverbose --verbose (Display of output, verbosly)\nlr --lr learningrate (Learning rate)\nrs --rs (Regularization strength)"
+#define COMMAND "h -h help --help ? /? (Displays help screen)\n\
+v -v version --version /v (Displays version number)\n\
+e epoch --epoch /e (Sets epoch or number of times the training loop would run)\n\
+corpus --corpus (Path to the file which has the training data)\n\
+verbose --verbose (Display of output, verbosly)\n\
+lr --lr learningrate (Learning rate)\n\
+rs --rs (Regularization strength)\n"
 
 #endif
